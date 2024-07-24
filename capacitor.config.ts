44fa3daaa5a19b2878a1bef9681ee7f00c6618de
João Silva <61765922+CarDioLogic@users.noreply.github.com>
2024-07-24 17:01:29 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyGamingJournalR',
+  webDir: 'www'
+};
+
+export default config;
